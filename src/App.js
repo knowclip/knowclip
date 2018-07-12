5338f3e2a70a2be08a107b7381b893a3c22f7bda
Justin Silvestre <justinsilvestre@gmail.com>
2018-07-12 23:43:44 +0200

    MVP

--- src/App.js
+++ src/App.js
@@ -1,18 +1,163 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ShowAll from './ShowAll'
+import { TextField, Button } from '@material-ui/core'
+
+const localFlashcardKey = (file) => `${file.type}_____${file.name}`
+const setLocalFlashcard = (flashcard) => {
+  const { localStorage } = window
+  if (localStorage) {
+    const serializedFlashcardData = JSON.stringify({ en: flashcard.en, de: flashcard.de })
+    localStorage.setItem(localFlashcardKey(flashcard.file), serializedFlashcardData)
+  }
+}
+
+const getLocalFlashcard = (file) => {
+  const { localStorage } = window
+  if (localStorage) {
+    const local = localStorage.getItem(localFlashcardKey(file))
+    return local ? { ...JSON.parse(local), file } : null
+  }
+}
+
+const getFlashcards = (files) => {
+  const map = {};
+  files.forEach(file => {
+    const local = getLocalFlashcard(file)
+    map[file.name] = local || { de: '', en: '', file }
+  })
+  return map
+}
+
 
 class App extends Component {
+  state = {
+    files: [],
+    currentFileIndex: 0,
+    flashcardsData: {},
+    modalIsOpen: false,
+  }
+
+  setFiles = (e) => {
+    const files = [...e.target.files]
+    this.setState({ files, flashcardsData: getFlashcards(files) }, () => this.germanInput.focus())
+    this.playAudio(e.target.files[0])
+  }
+
+  triggerFileInputClick = () => {
+    this.fileInput.click()
+  }
+
+  fileInputRef = (el) => this.fileInput = el
+  audioRef = (el) => this.audio = el
+  germanInputRef = (el) => this.germanInput = el
+
+  playAudio = (file) => {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      this.audio.src = e.target.result
+      this.audio.play()
+    }
+    reader.readAsDataURL(file)
+  }
+
+  goToFile = (index) => {
+    this.setState({ currentFileIndex: index })
+    this.playAudio(this.state.files[index])
+  }
+  prevFile = () => {
+    const lower = this.state.currentFileIndex - 1
+    this.goToFile(lower >= 0 ? lower : 0)
+  }
+  nextFile = () => {
+    const higher = this.state.currentFileIndex + 1
+    this.goToFile(higher <= this.state.files.length - 1 ? higher : 0)
+  }
+  handleFlashcardSubmit = (e) => {
+    e.preventDefault()
+    this.nextFile()
+    this.germanInput.focus()
+  }
+
+  setFlashcardText = (key, text) => {
+    const newFlashcard = {
+      ...this.getCurrentFlashcard(),
+      [key]: text,
+    }
+    const flashcardsData = {
+      ...this.state.flashcardsData,
+      [this.getCurrentFlashcard().file.name]: newFlashcard
+    }
+    this.setState({ flashcardsData })
+    setLocalFlashcard(newFlashcard)
+  }
+
+  setGerman = (e) => this.setFlashcardText('de', e.target.value)
+  setEnglish = (e) => this.setFlashcardText('en', e.target.value)
+
+  getCurrentFile = () => this.state.files[this.state.currentFileIndex]
+
+  getCurrentFlashcard = () => this.state.flashcardsData[this.getCurrentFile().name]
+
+  getGerman = () => this.getCurrentFlashcard().de
+  getEnglish = () => this.getCurrentFlashcard().en
+
+  areFilesLoaded = () => Boolean(this.state.files.length)
+  isNextButtonEnabled = () => this.state.currentFileIndex === this.state.files.length - 1
+  isPrevButtonEnabled = () => this.state.currentFileIndex === 0
+  isModalOpen = () => this.state.modalIsOpen
+
+  openModal = () => this.setState({ modalIsOpen: true })
+  closeModal = () => this.setState({ modalIsOpen: false })
+
   render() {
+    const form = this.areFilesLoaded()
+      ? <form className="form" onSubmit={this.handleFlashcardSubmit}>
+        <audio loop ref={this.audioRef} controls className="audioPlayer" autoplay></audio>
+        <p className="audioFilenameMenu">
+          <Button onClick={this.prevFile} disabled={this.isPrevButtonEnabled()}>Previous</Button>
+          <h2 className="audioFileName">
+            {this.getCurrentFlashcard().file.name}
+          </h2>
+          <Button onClick={this.nextFile} disabled={this.isNextButtonEnabled()}>Next</Button>
+        </p>
+        <div className="formBody">
+          <p lang="de">
+            <TextField inputRef={this.germanInputRef} onChange={this.setGerman} value={this.getGerman()} fullWidth multiline label="German" /></p>
+          <p lang="en">
+            <TextField onChange={this.setEnglish} value={this.getEnglish()} fullWidth multiline label="English" />
+          </p>
+          <Button type="submit" fullWidth onClick={this.submitFlashcardForm} disabled={this.isNextButtonEnabled()}>
+            Continue
+          </Button>
+          <Button fullWidth onClick={this.openModal}>Review &amp; export</Button>
+        </div>
+      </form>
+      : <p>
+        Select audio files from your <a href="https://apps.ankiweb.net/docs/manual.html#files">Anki collection.media folder</a> to start making flashcards.
+      </p>
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Audio Flashcard Assistant</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          <Button label="files" onClick={this.triggerFileInputClick}>
+            <input className="fileInput" multiple ref={this.fileInputRef} type="file" onChange={this.setFiles} />
+          </Button>
         </p>
+        {form}
+        <ShowAll
+          open={this.isModalOpen()}
+          handleClose={this.closeModal}
+          flashcardsData={this.state.flashcardsData}
+          files={this.state.files}
+          currentFileIndex={this.state.currentFileIndex}
+          goToFile={this.goToFile}
+        />
       </div>
     );
   }
---
+++ src/ShowAll.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Dialog, DialogActions, Table, TableBody, TableRow, TableCell, Button } from '@material-ui/core'
+import { unparse } from 'papaparse'
+
+const getFieldStyles = (string) => {
+  const styles = { }
+  if (!string.trim().length) styles.color = 'red'
+  return styles
+}
+
+const Field = ({ text }) => <span style={getFieldStyles(text)}>
+  {text || 'blank'}
+</span>
+
+const FlashcardRow = ({ flashcard: { file, de, en }, index, goToFile }) =>
+  <TableRow hover onClick={() => goToFile(index)}>
+    <TableCell style={{ maxWidth: '8em', overflow: 'hidden', textOverflow: 'ellipsis' }}>{file.name}</TableCell>
+    <TableCell><Field text={de} /></TableCell>
+    <TableCell><Field text={en} /></TableCell>
+  </TableRow>
+
+export default class ShowAll extends Component {
+  exportCsv = () => {
+    const usableFlashcards = this.props.files
+      .map(file => this.props.flashcardsData[file.name])
+      .filter(({ de, en }) => de.trim() || en.trim())
+      .map(({ en, de, file }) => [de, en, `[sound:${file.name}]`])
+    // TODO: alert if no usable
+    let csv = unparse(usableFlashcards)
+    const filename = 'export.csv';
+    console.log(csv)
+    if (!csv.match(/^data:text\/csv/i)) {
+        csv = 'data:text/csv;charset=utf-8,' + csv;
+    }
+    const data = encodeURI(csv);
+
+    const link = document.createElement('a');
+    link.setAttribute('href', data);
+    link.setAttribute('download', filename);
+    link.click();
+  }
+
+  render() {
+    const { open, handleClose, flashcardsData, files, currentFileIndex, goToFile } = this.props
+    return <Dialog open={open} onClose={handleClose}>
+      <Table>
+        <TableBody>
+          {files.map((file, i) =>
+            <FlashcardRow
+              key={file.name}
+              goToFile={goToFile}
+              flashcard={flashcardsData[file.name]}
+              isCurrent={currentFileIndex === i}
+              index={i}
+            />
+          )}
+        </TableBody>
+      </Table>
+      <DialogActions>
+        <Button onClick={this.exportCsv}>Export CSV file</Button>
+      </DialogActions>
+    </Dialog>
+  }
+}
