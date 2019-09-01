0994f0f46260dbe9922c405c4fa70ed29e57cfde
Justin Silvestre <justinsilvestre@gmail.com>
2019-09-01 11:23:11 +0200

    Implement sanzijing one-off markdown generation script

---
+++ src/utils/sanzijingMarkdown.js
@@ -0,0 +1,44 @@
+// @flow
+import * as r from '../redux'
+
+const flatten = [(a, b) => a.concat(b), []]
+
+const projectToMarkdown = (
+  state: AppState,
+  projectId: ProjectId,
+  noteType: NoteType
+): string => {
+  const projectMetadata = r.getProjectMetadata(state, projectId)
+  if (!projectMetadata) throw new Error('Could not find project')
+
+  const mediaMetadata = r.getProjectMediaMetadata(state, projectId)
+
+  return ([
+    ...mediaMetadata
+      .map(metadata => {
+        return [
+          ...r
+            .getClips(state, metadata.id)
+            .map((clip, i) => {
+              const { fields } = clip.flashcard
+              if (!fields.pronunciation)
+                throw new Error('Pronunciation field missing')
+              return [
+                `# ${clip.flashcard.tags.join(' ')}`,
+                `## 三字經 ${i}`,
+                fields.pronunciation.replace('/\n/g', '  \n'),
+                fields.transcription.replace('/\n/g', '  \n'),
+                fields.meaning.replace('/\n/g', '  \n'),
+                ...(fields.notes
+                  ? ['> ' + fields.notes.replace(/\n/g, '  \n> ')]
+                  : []),
+              ].join('\n\n')
+            })
+            .reduce(...flatten),
+        ]
+      })
+      .reduce(...flatten),
+  ]: Array<string>).join('\n')
+}
+
+export default projectToMarkdown
