76aa036a4d1c9b5a5bde6f6975fcad5bfcd68e00
Justin Silvestre <justinsilvestre@gmail.com>
2020-01-23 09:29:55 +0100

    Use prebuilt image from Docker Hub

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,10 +1,8 @@
-sudo docker build -t builder-node-12-wine ./docker
-
 sudo docker run --rm -ti --env ELECTRON_CACHE="/root/.cache/electron" \
   --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
   -v ${PWD}:/project \
   -v ${PWD##*/}-node-modules:/project/node_modules \
   -v ~/.cache/electron:/root/.cache/electron \
   -v ~/.cache/electron-builder:/root/.cache/electron-builder \
-  builder-node-12-wine \
+  justinsilvestre/electron-builder-node-12-wine \
   /bin/bash -c "yarn && yarn dist $*"
