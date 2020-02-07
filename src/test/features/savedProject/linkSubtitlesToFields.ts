c7495738656edaddfa81e7f8a2409161cbf1d774
Justin Silvestre <justinsilvestre@gmail.com>
2020-02-07 12:22:31 +0100

    Fix test

--- src/test/features/savedProject/linkSubtitlesToFields.ts
+++ src/test/features/savedProject/linkSubtitlesToFields.ts
@@ -12,11 +12,10 @@ export default async function linkSubtitlesToFields({
     mediaFilesMenu$.mediaFileMenuItem
   )
   await polarBearCafe.click()
-  await client.waitUntil(
-    async () =>
-      (await client.getAttribute('video', 'src')) === 'polar_bear_cafe.mp4'
+  await client.waitForText_(
+    mediaFilesMenu$.openMediaFilesMenuButton,
+    'polar_bear_cafe.mp4'
   )
-
   await client.clickElement_(waveform$.waveformClip)
 
   await client.clickElement_(flashcardFormFieldMenu$.openMenuButton)
