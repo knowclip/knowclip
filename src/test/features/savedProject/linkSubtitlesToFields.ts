985fe5c6f99d46d82f61efca8648c54b2a2e8e6d
Justin Silvestre <justinsilvestre@gmail.com>
2020-03-18 23:36:47 +0100

    Try fixing test

--- src/test/features/savedProject/linkSubtitlesToFields.ts
+++ src/test/features/savedProject/linkSubtitlesToFields.ts
@@ -20,6 +20,7 @@ export default async function linkSubtitlesToFields({
     'polar_bear_cafe.mp4'
   )
   await client.clickElement_(waveform$.waveformClip)
+  await client.waitForText_(flashcardSection$.container, '1 / 3')
 
   await client.clickElement_(flashcardFieldMenu$.openMenuButtons)
   await client.elements_(flashcardFieldMenu$.menuItem, 2)
