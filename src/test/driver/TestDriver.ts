1bdaa9f4b5a67ca3464e1094eb5cf6ff7140a736
Justin Silvestre <justinsilvestre@gmail.com>
2020-09-19 02:44:54 +0200

    Pass chromedriver args fully and remove backslashes from appveyor script

--- src/test/driver/TestDriver.ts
+++ src/test/driver/TestDriver.ts
@@ -117,6 +117,7 @@ export async function createTestDriver({
         binary: path,
         args: [
           'app=' + process.cwd(),
+          ...chromedriverArgs,
           // '--disable-extensions',
           // '--debug'
         ],
