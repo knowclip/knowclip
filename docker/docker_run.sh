06c9625f5d368d6b42cf938dd20d9745fec8fecd
Justin Silvestre <justinsilvestre@gmail.com>
2021-01-30 13:03:13 +0100

    Remove travis artifacts

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,6 +1,6 @@
 sudo docker run --rm -ti --env ELECTRON_CACHE="/root/.cache/electron" \
   --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
-  --env TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER \
+  --env GITHUB_RUN_NUMBER=$GITHUB_RUN_NUMBER \
   -v ${PWD}:/project \
   -v ${PWD##*/}-node-modules:/project/node_modules \
   -v ~/.cache/electron:/root/.cache/electron \
