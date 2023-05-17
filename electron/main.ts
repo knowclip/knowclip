ecfd4fb9d0574aaac655c709168627704ad11c5a
justinsilvestre <justinsilvestre@gmail.com>
2023-05-17 11:16:38 +0200

    See if electron-store import style affects windows integration test

--- electron/main.ts
+++ electron/main.ts
@@ -1,7 +1,6 @@
 import { BrowserWindow, screen, app, ipcMain, protocol } from 'electron'
 import * as path from 'path'
 import * as url from 'url'
-import { initRenderer } from 'electron-store'
 import setUpMenu from './appMenu'
 import installDevtools from './devtools'
 import { ROOT_DIRECTORY } from './root'
@@ -18,7 +17,7 @@ const isTesting = process.env.VITEST
 
 if (isTesting) interceptLogs()
 
-initRenderer()
+require('electron-store').initRenderer()
 
 const Sentry = require('@sentry/electron')
 
