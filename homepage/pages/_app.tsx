0594344093223844c5685bae6c178cae836568c8
justinsilvestre <justinsilvestre@gmail.com>
2023-05-17 17:47:05 +0200

    Try fixing build after next plugin update

--- homepage/pages/_app.tsx
+++ homepage/pages/_app.tsx
@@ -1,33 +1,38 @@
-import React, { useEffect } from 'react'
-import { initAnalytics } from '@pinjollist/next-with-analytics';
-import { useRouter } from 'next/router';
-import './_app.scss'
+import React, { useEffect } from "react"
+import { initAnalytics } from "@pinjollist/next-with-analytics"
+import { useRouter } from "next/router"
+import "./_app.scss"
 
 const options = {
   trackingCode: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID,
   respectDNT: true,
   anonymizeIp: true,
-};
+}
 
-const analyticsInstance = initAnalytics(options);
+const analyticsInstance = initAnalytics(options)
 
-export default function App({ Component, pageProps }: { Component: React.ComponentType, pageProps: any }) {
+export default function App({
+  Component,
+  pageProps,
+}: {
+  Component: React.ComponentType
+  pageProps: any
+}) {
   const router = useRouter()
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      const { handleRouteChange } = analyticsInstance;
-  
+    if (process.env.NODE_ENV === "production") {
+      const { handleRouteChange } = analyticsInstance
+
       // Enable tracking page views for route change
-      router.events.on('routeChangeComplete', handleRouteChange);
-  
-       // Disable tracking page views for route change before unmount
-      return () => router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.on("routeChangeComplete", handleRouteChange)
+
+      // Disable tracking page views for route change before unmount
+      return () => router.events.off("routeChangeComplete", handleRouteChange)
     }
-  }, [])
+  }, [router.events])
   useEffect(() => {
-    document.querySelector('html')
-      ?.classList.remove('noJs')
+    document.querySelector("html")?.classList.remove("noJs")
   }, [])
-  
+
   return <Component {...pageProps} />
 }
