192a67d5085fc4c1733e2db0500d2dc9d826d24f
justinsilvestre <justinsilvestre@gmail.com>
2023-05-17 15:21:31 +0200

    Undo wrap interceptLogs in try block

--- electron/interceptLogs.ts
+++ electron/interceptLogs.ts
@@ -4,25 +4,22 @@ import filenamify from 'filenamify'
 
 export function interceptLogs() {
   try {
-    const originalConsoleLog = console.log
-    const originalConsoleError = console.error
-    const logFilePath = path.join(
-      process.cwd(),
-      `electron-${filenamify(new Date(Date.now()).toISOString())}.log`
+  const originalConsoleLog = console.log
+  const originalConsoleError = console.error
+  const logFilePath = path.join(
+    process.cwd(),
+    `electron-${filenamify(new Date(Date.now()).toISOString())}.log`
+  )
+  writeFileSync(logFilePath, '')
+  console.log = (...args: any[]) => {
+    appendFileSync(logFilePath, JSON.stringify(args, null, 2))
+    originalConsoleLog(...args)
+  }
+  console.error = (...args: any[]) => {
+    appendFileSync(
+      logFilePath,
+      `           ERROR: ${JSON.stringify(args, null, 2)}`
     )
-    writeFileSync(logFilePath, '')
-    console.log = (...args: any[]) => {
-      appendFileSync(logFilePath, JSON.stringify(args, null, 2))
-      originalConsoleLog(...args)
-    }
-    console.error = (...args: any[]) => {
-      appendFileSync(
-        logFilePath,
-        `           ERROR: ${JSON.stringify(args, null, 2)}`
-      )
-      originalConsoleError(...args)
-    }
-  } catch (err) {
-    console.error(err)
+    originalConsoleError(...args)
   }
 }
