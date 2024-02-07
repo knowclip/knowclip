b125ffefddbdaaf492e9636cfc0cee78f61f356f
Justin Silvestre <justinsilvestre@gmail.com>
2024-02-07 12:44:15 +0100

    Nextjs upgrade 2024 netlify esbuild compatibility (#731)
    * Add back homepage package-lock
    
    * Trigger rebuild
    
    * Fix homepage styles
    
    * Try moving esbuild to devDependencies

--- homepage/components/header.tsx
+++ homepage/components/header.tsx
@@ -3,27 +3,22 @@ import css from "./header.module.css"
 import cn from "classnames"
 import Link from "next/link"
 
-const Header = ({ siteTitle }: { siteTitle: string }) => (
+const Header = ({ siteTitle = "" }: { siteTitle: string }) => (
   <header className={css.container}>
     <h1 className={cn(css.heading, css.filmBackground)}>
       <img src="/lightbulb.png" className={css.lightbulbIcon} alt="" />
-      <Link href="/" passHref>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </a>
+      <Link
+        href="/"
+        passHref
+        style={{
+          color: `white`,
+          textDecoration: `none`,
+        }}
+      >
+        {siteTitle}
       </Link>
     </h1>
   </header>
 )
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
