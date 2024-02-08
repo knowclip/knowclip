ddb18a91d023b3d24fd4e06f301abb2212b21b57
justinsilvestre <justinsilvestre@gmail.com>
2024-02-08 11:09:04 +0100

    Fix afterUpdates

--- src/utils/afterUpdates.ts
+++ src/utils/afterUpdates.ts
@@ -4,5 +4,6 @@ import { mergeAll } from 'rxjs/operators'
 
 export const afterUpdates = <T>(callback: () => Promise<Observable<T>>) =>
   defer(async () => {
+    await null
     return await flushSync(() => callback())
   }).pipe(mergeAll())
